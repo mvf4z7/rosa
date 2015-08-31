ca2b9fbc603c1645ee37634c6dd20e4de89c680a
mvf4z7 <mike.fanger@gmail.com>
2015-08-31 17:09:46 -0500

    moved LeftNav code into separate component called NavDrawer, cleaned up old code from boilerplate - MF

--- server_and_app/src/views/app/app.jsx
+++ server_and_app/src/views/app/app.jsx
@@ -1,41 +1,24 @@
 import React from 'react';
-import { RouteHandler, Link } from 'react-router';
-
-//import Header from '../../components/header/header';
-
+import { RouteHandler } from 'react-router';
 import mui from 'material-ui';
-let Colors = mui.Styles.Colors;
+
+import { AppBar } from 'material-ui';
+import NavDrawer from '../../components/nav-drawer/nav-drawer';
+
+import styles from './styles';
+require('./app.scss');
+
 let ThemeManager = new mui.Styles.ThemeManager();
-
-import { AppBar, LeftNav } from 'material-ui';
-
-let styles = {
-    AppBar: {
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        backgroundColor: Colors.greenA700,
-    },
-
-    LeftNav: {
-        color: Colors.greenA700
-    }
-};
 
 let menuItems = [
     { route: '/', text: 'home' },
     { route: '/info', text: 'info' }
 ]
 
-require('./app.scss');
-
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { selectedIndex: 0 };
-
         this._toggleNav = this._toggleNav.bind(this);
-        this._onLeftNavChange = this._onLeftNavChange.bind(this);
     }
 
     getChildContext() {
@@ -50,14 +33,8 @@ class App extends React.Component {
                 <AppBar
                     title='ROSA'
                     style={styles.AppBar}
-                    onLeftIconButtonTouchTap={this._toggleNav}/>
-                <LeftNav
-                    ref='leftNav'
-                    docked={false}
-                    menuItems={menuItems}
-                    selectedIndex={this.state.selectedIndex}
-                    onChange={this._onLeftNavChange}
-                    style={styles.LeftNav} />
+                    onLeftIconButtonTouchTap={this._toggleNav} />
+                <NavDrawer ref='navDrawer' menuItems={menuItems}/>
                 <RouteHandler />
     		</div>
         );
@@ -65,21 +42,12 @@ class App extends React.Component {
 
     _toggleNav(e) {
         e.preventDefault();
-        this.refs.leftNav.toggle();
-    }
-
-    _onLeftNavChange(e, selectedIndex, menuItem) {
-        this.context.router.transitionTo(menuItem.route);
-        this.setState({selectedIndex: selectedIndex})
+        this.refs.navDrawer.toggle();
     }
 }
 
 App.childContextTypes = {
     muiTheme: React.PropTypes.object
-};
-
-App.contextTypes = {
-    router: React.PropTypes.func
 };
 
 export default App;
---
+++ server_and_app/src/views/app/styles.js
@@ -0,0 +1,13 @@
+import mui from 'material-ui';
+let Colors = mui.Styles.Colors;
+
+let styles = {
+    AppBar: {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        backgroundColor: Colors.greenA700,
+    },
+};
+
+export default styles;
