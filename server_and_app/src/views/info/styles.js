5dfd3dffa678f1502a35541ed2aa3205c23a377a
mvf4z7 <mike.fanger@gmail.com>
2015-09-20 19:36:03 -0500

    Added layout for info page, still working on content - MF

--- server_and_app/src/views/info/styles.js
+++ server_and_app/src/views/info/styles.js
@@ -1,4 +1,34 @@
 let styles = {
+    header: {
+        textAlign: 'center',
+        margin: '2rem 0 1rem 0',
+        fontSize: '4rem',
+        color: '#0A5CBF'
+    },
+    tagLine: {
+        textAlign: 'center',
+        fontSize: '1.5rem',
+    },
+    accent: {
+        color: '#0A5CBF'
+    },
+    boxContainer: {
+        marginTop: '4rem'
+    },
+    box: {
+        textAlign: 'center',
+        padding: '1em',
+        borderColor: 'black',
+        borderStyle: 'dashed'
+    },
+    boxTitle: {
+        color: '#0A5CBF',
+        marginBottom: '10px',
+        textAlign: 'left'
+    },
+    boxBody: {
+        textAlign: 'center'
+    },
     button: {
         margin: '1em'
     },
