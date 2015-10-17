615b655610295e7e0a497c928a569a0efe63532a
mvf4z7 <mike.fanger@gmail.com>
2015-10-16 23:45:58 -0500

    replaced flex box styling on start-stop-btn with top and bottom margin. -MF

--- server_and_app/src/components/start-stop-btn/start-stop-btn.jsx
+++ server_and_app/src/components/start-stop-btn/start-stop-btn.jsx
@@ -59,13 +59,10 @@ export default Radium(StartStopBtn);
 
 let styles = {
     container: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
         position: 'absolute',
         bottom: 0,
         width: '100%',
-        height: '10%',
+        padding: '0.9rem 0',
         textAlign: 'center',
         fontSize: '2rem',
         color: 'white',
