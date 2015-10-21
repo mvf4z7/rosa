d2763bce56fe0f2915c59df2f494a9b4dce24666
mvf4z7 <mike.fanger@gmail.com>
2015-10-21 00:14:02 -0500

    Cleaned up webpack config and refactored /ovensim api endpoint to account for dev and production scenarios. -MF

--- server_and_app/webpack.config.js
+++ server_and_app/webpack.config.js
@@ -2,46 +2,65 @@ var webpack = require('webpack');
 var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 var path = require('path');
 
-var isDev = process.env.NODE_ENV !== 'production';
-var entry = ['./src/main'];
-var output = {
-    path: path.join(__dirname, 'build'),
-    filename: 'bundle.js'
-};
-var plugins = [new UglifyJsPlugin({ minimize: true })];
-
-if(isDev) {
-    entry = [
-        'webpack-dev-server/client?http://localhost:3001',
-        'webpack/hot/only-dev-server',
-        './src/main'
-    ];
-    output.publicPath = 'http://localhost:3001/build';
-    plugins = [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin(),
-        new UglifyJsPlugin({ minimize: true })
-    ]
-}
+var env = process.env.NODE_ENV === 'production' ? 'prod' : 'dev';
 
 var config = {
-    devtool: 'eval',
-    entry: entry,
-    output: output,
-    resolve: {
-        extensions: ['', '.js', '.jsx']
+    dev: {
+        devtool: 'eval',
+        entry: [
+            'webpack-dev-server/client?http://localhost:3001',
+            'webpack/hot/only-dev-server',
+            './src/main'
+        ],
+        output: {
+            path: path.join(__dirname, 'build'),
+            filename: 'bundle.js',
+            publicPath: 'http://localhost:3001/build'
+        },
+        resolve: {
+            extensions: ['', '.js', '.jsx']
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.NoErrorsPlugin(),
+        ],
+        module: {
+            loaders: [{
+                test: /\.jsx?$/,
+                loaders: ['react-hot', 'babel'],
+                include: path.join(__dirname, 'src')
+            }, {
+                test: /\.scss$/,
+                loader: 'style!css!sass'
+            }]
+        }
     },
-    plugins: plugins,
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            loaders: ['react-hot', 'babel'],
-            include: path.join(__dirname, 'src')
-        }, {
-            test: /\.scss$/,
-            loader: 'style!css!sass'
-        }]
+    prod: {
+        devtool: 'eval',
+        entry: [
+            './src/main'
+        ],
+        output: {
+            path: path.join(__dirname, 'build'),
+            filename: 'bundle.js',
+        },
+        resolve: {
+            extensions: ['', '.js', '.jsx']
+        },
+        plugins: [
+            new UglifyJsPlugin({ minimize: true })
+        ],
+        module: {
+            loaders: [{
+                test: /\.jsx?$/,
+                loaders: ['react-hot', 'babel'],
+                include: path.join(__dirname, 'src')
+            }, {
+                test: /\.scss$/,
+                loader: 'style!css!sass'
+            }]
+        }
     }
-};
+}
 
-module.exports = config;
+module.exports = config[env];
