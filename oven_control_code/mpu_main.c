b810f95c1b094966758b797d1ef59f7859c6f92b
Eftink <jhezq7@mst.edu>
2015-10-21 10:15:20 -0500

    Added PID control algorithm.

--- oven_control_code/mpu_main.c
+++ oven_control_code/mpu_main.c
@@ -4,12 +4,17 @@
 #include <unistd.h>
 #include "stdio.h"
 #include "pruio.h" // include header
+#include "pruio_pins.h"
 
 #include "mpu_types.h"
 #include "mpu_util.h"
 #include "mpu_timer.h"
+#include "mpu_pid.h"
 
 #define INC_INDEX( i, max );     i++; if( i >= max ) i = 0;
+
+#define P_OUT     P9_21
+#define PWM_FREQ  0.5f
 
 boolean force_stop;
 
@@ -21,9 +26,11 @@ int main( int argc, char *argv[] )
     float temp;
     float voltage;
     float amp_voltage;
-    profile prf;
     uint32 cur_time;
     uint32 start_time;
+    float duty_cycle;
+    float target_temp;
+    char print_string[ 200 ];
     
     force_stop = FALSE;
     
@@ -31,45 +38,57 @@ int main( int argc, char *argv[] )
     
     if( argc != 2 )
     {
-        printf( "Error: The only argument is the path to the JSON file.\n" );
+        util_print_debug( "Error: The only argument is the path to the JSON file.\n" );
         return( -1 );
     }
     
     //Set up signal handler:
     if( signal( SIGINT, signalHandler ) == SIG_ERR )
     {
-        printf( "MPU: Error setting up ctrl-c interrupt.\n" );
+        util_print_debug( "MPU: Error setting up ctrl-c interrupt.\n" );
         return( -1 );
     }
     
-    if( !util_load_profile( argv[ 1 ], &prf ) )
+    if( !pid_init( argv[ 1 ] ) )
     {
-        printf( "Error: There was an error loading the profile.\n" );
+        util_print_debug( "Error: There was an error loading the profile.\n" );
         return( -1 );
     }
    
     pruIo *io = pruio_new(PRUIO_DEF_ACTIVE, 0x98, 0, 1); 
-    if (pruio_config(io, 1, 0x1FE, 0, 4))
-    { 
-        printf("config failed (%s)\n", io->Errr);
-        return( -1 );
-    }
 
     start_time = 0;
     cur_time = 0;
+    duty_cycle = 1.0;
+    pruio_pwm_setValue( io, P_OUT, PWM_FREQ, duty_cycle );
+    
+    if( pruio_config( io, 1, 0x1FE, 0, 4 ) ) 
+    {       
+        sprintf( print_string, "config failed (%s)\n", io->Errr );
+        util_print_debug( print_string ); 
+        return( -1 );
+    }
     
     while( !force_stop )
     {
         
-        printf( "%s\n\n", util_get_json_string( cur_time, temp, temp - 1 ) );
+        
         fflush( stdout );
         adc_val = io->Adc->Value[ 1 ];
         voltage = (float)( adc_val ) / 0xFFF0 * 1.8;
         amp_voltage = 2 * voltage;
         temp = ( amp_voltage - 1.25 ) / 0.005;
         
-        //block for 1 second.
-        while( cur_time - start_time <= 1000 )
+        target_temp = pid_find_target( cur_time / 1000.0 );
+        
+        duty_cycle = pid_calc( target_temp, temp );
+        
+        pruio_pwm_setValue( io, P_OUT, -1, duty_cycle );
+        
+        util_print_point( cur_time / 1000, target_temp, temp );
+               
+        //block for 2 seconds.
+        while( cur_time - start_time <= 2000 )
         {
             if( !timer_get( &cur_time ) )
             {
@@ -78,7 +97,7 @@ int main( int argc, char *argv[] )
             }
         }
         
-        start_time = start_time + 1000;
+        start_time = start_time + 2000;
     }   
     
     pruio_destroy(io);        /* destroy driver structure */
--- oven_control_code/mpu_util.h
+++ oven_control_code/mpu_util.h
@@ -9,19 +9,10 @@ enum
     DEBUG_MSG
 };
 
-//This function will convert the string parameter with the format: "0xdddddddd"
-//into its integer representation.
-boolean util_str_to_hex( const char * str, uint32 * ret_val );
-
-//This function will open the .json file at path and load the object into shared
-//memory allocated specifically for this purpose. If it returns false there was
-//an error.
-boolean util_load_profile( const char * path, profile * mem );
-
 //Function for generating a JSON string, based on the given parameter values.
 //Returns a readable string in JSON format.
-char* util_get_json_string(uint32 time, float target, float temperature);
+void util_print_point(uint32 time, float target, float temperature);
 
-char * util_print_debug( const char * string );
+void util_print_debug( const char * string );
 
 #endif
