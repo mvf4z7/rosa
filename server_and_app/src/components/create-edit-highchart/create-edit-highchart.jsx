24e6393392221c24e4f5abf464c6d1e4f892dec2
mvf4z7 <mike.fanger@gmail.com>
2015-10-24 17:53:32 -0500

    WIP - created component for create temp profiles - MF

---
+++ server_and_app/src/components/create-edit-highchart/create-edit-highchart.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import Radium from 'radium';
+import * as _ from 'lodash';
+import globalChartConfig from '../../highcharts';
+
+import Highcharts from 'react-highcharts';
+import Spinner from '../spinner/spinner';
+
+var chartConfig = _.clone(globalChartConfig, true);
+
+var overRides = {
+    chart: {
+        animation: {
+            duration: 100,
+            easing: 'linear'
+        },
+        zoomType: 'x'
+    },
+    title: {
+        text: 'Click the "+" to add data points'
+    },
+    xAxis: {
+        title: {
+            text: 'time (s)',
+            style: { 'fontSize': '1rem' },
+            y: 20
+        },
+        min: 0
+    },
+    yAxis: {
+        title: {
+            text: 'temperature (C)',
+            style: { 'fontSize': '1rem' }
+        }
+    },
+    legend: { x: 30 },
+    toolTip: {
+        shared: true,
+        crosshairs: true
+    },
+    plotOptions: {
+        line: { animation: false }
+    },
+    credits: { enabled: false },
+    series: [{
+        name: '',
+        data: [[0, 23]]
+    }]
+}
+
+chartConfig = _.merge(chartConfig, overRides);
+
+class CreateEditHighchart extends React.Component {
+    render() {
+        if(this.props.loading) {
+            return ( <Spinner /> );
+        }
+
+        return(
+            <div id='createchartwrapper' style={styles.chartWrapper}>
+                <Highcharts ref='chart' config={chartConfig} style={styles.highChart}></Highcharts>
+            </div>
+        );
+    }
+
+}
+
+let styles = {
+    chartWrapper: {
+        height: '55vh',
+    },
+    highChart: {
+        height: '100%'
+    }
+};
+
+export default Radium(CreateEditHighchart);
