1da20ac383c17dd591347748012ea892e58e2bab
mvf4z7 <mike.fanger@gmail.com>
2015-11-11 00:08:41 -0600

    WIP - Displaying multiple cards from prepopuulated create profile store data - MF

--- server_and_app/src/components/data-point-card/data-point-card.jsx
+++ server_and_app/src/components/data-point-card/data-point-card.jsx
@@ -1,20 +1,45 @@
 import React from 'react';
 import Radium from 'radium';
 
+import { TextField } from 'material-ui';
+
 class DataPointCard extends React.Component {
     constructor(props) {
         super(props);
     }
 
+    componentDidMount() {
+        if(!this.props.point.length) {
+            return;
+        }
+
+        this.refs.time.setValue(this.props.point[0]);
+        this.refs.temp.setValue(this.props.point[1]);
+    }
+
     render() {
         return (
             <div style={styles.container}>
-                DataPointCard
+                <div style={styles.index}>{this.props.index}</div>
+                <TextField
+                    ref='time'
+                    floatingLabelText='Time ( s )'
+                    onEnterKeyDown={this._updateValue}
+                    style={styles.inputContainer} />
+                <TextField
+                    ref='temp'
+                    floatingLabelText='Temp ( C )'
+                    onEnterKeyDown={this._updateValue}
+                    style={styles.inputContainer} />
             </div>
         );
     }
 
-
+    _updateValue = () => {
+        console.log('enter key down');
+        this.refs.time.blur();
+        this.refs.temp.blur();
+    }
 }
 
 
@@ -22,10 +47,21 @@ class DataPointCard extends React.Component {
 let styles = {
     container: {
         display: 'inline-block',
+        position: 'relative',
         backgroundColor: '#FFFAFA',
         boxShadow: '0px 10px 6px -6px #777',
-        padding: '2rem',
-        margin: '1rem 1rem'
+        padding: '10px',
+        margin: '1rem 1rem',
+        textAlign: 'center'
+    },
+    index: {
+        textAlign: 'left',
+        fontSize: '0.8rem',
+        padding: '3px 12px'
+    },
+    inputContainer: {
+        margin: '0 1em',
+        width: '120px'
     }
 };
 
