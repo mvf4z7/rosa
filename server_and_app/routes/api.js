f1d6c45b3673d29c2949779ea50249276e205a70
epicpants <tjrg88@mst.edu>
2015-12-02 18:22:02 -0600

    Minor cleanup

--- server_and_app/routes/api.js
+++ server_and_app/routes/api.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-//var multer = require('multer');
 
 var history = require('./api/history');
 var oven = require('./api/oven');
@@ -9,23 +8,23 @@ var user = require('./api/user');
 
 router.route('/ovensim')
     .get(function(req, res) { oven.getOvenState(req, res) })
-    .put(function(req, res) { oven.startOven(req, res)})
+    .put(function(req, res) { oven.startOven(req, res) })
     .delete(function(req, res) { oven.stopOven(req, res) });
 
 router.route('/profiles')
     .get(function(req, res) { profile.getProfiles(req, res) })
-    .post(function(req, res) { profile.addProfile(req, res)});
+    .post(function(req, res) { profile.addProfile(req, res) });
 
 router.route('/profiles/:pname')
     .delete(function(req, res) { profile.removeProfile(req, res, req.params.pname) });
 
 router.route('/allusers')
-    .get(function(req, res) { user.getAllUsers(req, res)});
+    .get(function(req, res) { user.getAllUsers(req, res) });
 
 router.route('/adduser')
-    .post(function(req, res) { user.addUser(req, res)});
+    .post(function(req, res) { user.addUser(req, res) });
 
 router.route('/removeuser')
-    .post(function(req, res) { user.removeUser(req, res)});
+    .post(function(req, res) { user.removeUser(req, res) });
 
 module.exports = router;
